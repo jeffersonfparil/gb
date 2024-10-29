cc03950be5670b97c2905c5d3a88a3b98912342d
jeffersonfparil <jeffersonparil@gmail.com>
2024-10-29 14:06:27 +1100

    start drafting a simulation framework so that we can start building while testing

---
+++ io/simulation.py
@@ -0,0 +1,16 @@
+from typing import Union
+import numpy as np
+
+Vector = list[float]
+Matrix = list[list[float]]
+
+### Number of samples
+n: int = 10
+### Number of loci
+p: int = 42
+### Sample marker effects
+b: Vector = np.reshape(np.random.uniform(-10, 10, p), (p, 1))
+### Simulate linkage disequilibrium by sample from a multivariate normal distribution
+C: Matrix = np.matmul(b, b.T)
+X: Matrix = np.random.multivariate_normal(np.reshape(b, (p,)), C, (n, p), check_valid='raise').T
+X.shape
